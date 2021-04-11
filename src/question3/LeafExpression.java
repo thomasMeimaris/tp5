d4e326661e084ab61daa025caeae279cea0e0da1
thomasMeimaris <thomas.meimaris@gmail.com>
2021-04-11 21:04:59 +0200

    question3

--- src/question3/LeafExpression.java
+++ src/question3/LeafExpression.java
@@ -2,4 +2,18 @@ package question3;
 
 public class LeafExpression implements Expression {
 
+    private Integer value;
+
+    public int getValue(){
+        return this.value;
+    }
+
+    public LeafExpression(Integer value){
+        this.value = value;
+    }
+
+    @Override
+    public <T> T accept(Visitor<T> v) {
+        return (T) v.visit(this);
+    }
 }
--- src/question3/Visitor.java
+++ src/question3/Visitor.java
@@ -2,4 +2,8 @@ package question3;
 
 public abstract class Visitor<T> {
 
+    public abstract T visit(LeafExpression n);
+
+    public abstract T visit(Substraction s);
+
 }
