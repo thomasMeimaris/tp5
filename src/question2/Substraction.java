fcf1822f581cabdcbdc1d337d91de65f47fe3b1a
thomasMeimaris <thomas.meimaris@gmail.com>
2021-04-11 18:23:33 +0200

    question2

--- src/question2/Substraction.java
+++ src/question2/Substraction.java
@@ -2,4 +2,15 @@ package question2;
 
 public class Substraction implements Expression {
 
+    private Expression left, right;
+
+    public Substraction(Expression left, Expression right){
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public int interpreter() {
+        return left.interpreter() - right.interpreter();
+    }
 }
